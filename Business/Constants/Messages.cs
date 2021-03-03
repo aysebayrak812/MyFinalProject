f5f578f5211ef21c689f473a21e4ccfb8d5e61a6
Ayşe BAYRAK <202803003@ogr.cbu.edu.tr>
2021-03-03 20:50:40 +0300

    .

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,19 +1,28 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
 {
-  public  static class Messages//newlemeye gerek yok
+    public static class Messages//newlemeye gerek yok
     {
-        
-        public static string ProductAdded = "Ürün eklendi" ;
+
+        public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
-        public static string MaintenanceTime= "sistem bakımda";
-        public static string ProductsListed="ürünler listelendi";
+        public static string MaintenanceTime = "sistem bakımda";
+        public static string ProductsListed = "ürünler listelendi";
         public static string ProductCountOfCategoryError = "kategoryde 10 dan fazla ürün olamaz";
         public static string ProductNameAlreadyExists = "bu üründe zaten başka bir ürün var  ";
 
         public static string CategoryLimitExceded = "kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string AuthorizationDenied = "yetkiniz yok ";
+        public static string UserRegistered = "kayıt oldu";
+        public static string UserNotFound = "kullanıcı bulunamadı";
+        public static string PasswordError = "parola hatası";
+        public static string SuccessfulLogin = "başarılı giriş";
+        public static string UserAlreadyExists = "kullanıcı mevcut";
+        public static string AccessTokenCreated = "token oluşturuldu ";
     }
 }
---
+++ Core/Utilities/IoC/ServiceTool.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.IoC
+{
+    public static class ServiceTool
+    {
+        // injection yakalayabilmemeiz sağlar (mindovs form dada kullanmamızı şagladığımız yer
+        // injection alt yaoısını aynne okummamız ısağlar
+        //api de yada autofacdeki injection  leri oluşturabilmemizi sağlıyor
+
+        public static IServiceProvider ServiceProvider { get; private set; }
+
+        public static IServiceCollection Create(IServiceCollection services)
+        {
+            ServiceProvider = services.BuildServiceProvider();
+            return services;
+        }
+    }
+}
